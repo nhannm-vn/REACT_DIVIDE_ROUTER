21242978a75578f8e3050a41fc36081c7f942bb6
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-02-08 12:49:07 +0700

    update navlink and to and replace and isactive

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 interface Props {
   children?: React.ReactNode
@@ -10,28 +11,42 @@ export default function MainLayout({ children }: Props) {
         <div className='h-full overflow-y-auto bg-gray-100 py-4 px-3 shadow-lg'>
           <ul className='space-y-2'>
             <li>
-              <a
-                href=''
-                className='flex items-center rounded-lg bg-gray-300 p-2 text-base font-normal text-gray-900 hover:bg-gray-300'
+              <NavLink
+                // to sẽ giúp mình đi tới nơi mà mình muốn
+                to='/'
+                // replace sẽ giúp nó không lưu lại lịch sử
+                replace
+                className={({ isActive }) => {
+                  const activeClass = isActive ? 'bg-gray-300' : ''
+                  return `flex items-center rounded-lg  p-2 text-base ${activeClass} font-normal text-gray-900 hover:bg-gray-300`
+                }}
               >
                 <span className='ml-3 font-bold'>Dashboard</span>
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a
-                href=''
-                className='flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-300'
+              <NavLink
+                to='/staff'
+                replace
+                className={({ isActive }) => {
+                  const activeClass = isActive ? 'bg-gray-300' : ''
+                  return `flex items-center rounded-lg p-2 text-base ${activeClass} font-normal text-gray-900 hover:bg-gray-300`
+                }}
               >
                 <span className='ml-3'>Staff</span>
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a
-                href=''
-                className='flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-300'
+              <NavLink
+                to='/about'
+                replace
+                className={({ isActive }) => {
+                  const activeClass = isActive ? 'bg-gray-300' : ''
+                  return `flex items-center rounded-lg p-2 text-base ${activeClass} font-normal text-gray-900 hover:bg-gray-300`
+                }}
               >
                 <span className='ml-3'>About</span>
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
@@ -40,3 +55,12 @@ export default function MainLayout({ children }: Props) {
     </div>
   )
 }
+
+/**
+ * Lưu ý vì react quan tâm chính là việc render. Chính vì vậy mà mình rất ngại dùng thẻ a
+ * thay vào đó thì sẽ có hai thằng thay thế chính là Link và NavLink
+ */
+
+/**
+ * thẻ a bây giờ sẽ thay bằng NavLink và prop "to" sẽ đưa mình đến nới mà mình muốn đến
+ */
