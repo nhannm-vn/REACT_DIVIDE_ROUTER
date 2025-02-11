341cdf65729ca635568f8f6c84454564d0df50d7
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-02-11 09:34:52 +0700

    update nested route

--- src/components/StaffItem/StaffItem.tsx
+++ src/components/StaffItem/StaffItem.tsx
@@ -1,11 +1,12 @@
-import { useParams } from 'react-router-dom'
+import { useOutletContext, useParams } from 'react-router-dom'
 
 export default function StaffItem() {
   // Nếu ở đây mà mình muốn nhận được cái param từ url
   //thì phải thông qua hook này
   const { id } = useParams()
   console.log(id)
-
+  // const context = useOutletContext()
+  // console.log(context)
   // const params = useParams()
   // console.log(params)
   //staff/:id/:address
--- src/pages/Staff/Staff.tsx
+++ src/pages/Staff/Staff.tsx
@@ -1,7 +1,4 @@
-import AddStaff from 'components/AddStaff'
-import StaffList from 'components/StaffList'
-
-import { Link } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 export default function Staff() {
   return (
@@ -10,28 +7,27 @@ export default function Staff() {
       <div className='border-b border-gray-200 text-center text-sm font-medium text-gray-500  '>
         <ul className='-mb-px flex flex-wrap'>
           <li className='mr-2'>
-            <a
-              href='#'
+            <NavLink
+              to='/staff/list'
               className='inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 '
             >
               List
-            </a>
+            </NavLink>
           </li>
           <li className='mr-2'>
-            <a
-              href='#'
+            <NavLink
+              to='/staff/add'
               className='active inline-block rounded-t-lg border-b-2 border-blue-600 p-4 text-blue-600 '
               aria-current='page'
             >
               Add
-            </a>
+            </NavLink>
           </li>
         </ul>
       </div>
-
-      {/* <StaffList /> */}
-
-      {/* <AddStaff /> */}
+      {/* Này giúp hiển thị cho nested route hiển thị các children của route cha*/}
+      <Outlet context={{ profile: { name: 'Nhan' } }} />
+      {/* Những cái nội dung này sẽ nhận ở các component tương ứng thông qua hook */}
     </div>
   )
 }
