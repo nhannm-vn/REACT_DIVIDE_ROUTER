efb6d1400c8d0b825301316003583f6b83bc1153
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-02-14 11:53:55 +0700

    update component notfound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+/*
+  Navigate giúp khi tới trang lỗi thì quay về trang nào đó
+  hoặc sau bao lâu thì quay về đâu đó
+  Nếu không dùng thẻ Navigate thì có thể dùng hook useNavigate
+  trong navigate nếu truyền -1 thì sau một khoảng nó tự quay về trang trước đó
+*/
 
 function NotFound() {
-  return <div>NotFound</div>
+  const navigate = useNavigate()
+  useEffect(() => {
+    setTimeout(() => {
+      navigate(-1)
+    }, 2000)
+  }, [navigate])
+  return <div>Not Found</div>
 }
 
 export default NotFound
